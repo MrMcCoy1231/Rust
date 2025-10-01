dd6fd31024039ca55174fda3c4a0f4461a6921c0
MrMcCoy1231 <tudoralexie@gmail.com>
2025-10-01 19:19:57 +0300

    Exercitiile de la primul laborator

---
+++ hello_world/target/debug/deps/hello_world-6a1afbb608fd85cc.d
@@ -0,0 +1,5 @@
+C:\Users\Tudor Alexie\Desktop\Rust\hello_world\target\debug\deps\hello_world-6a1afbb608fd85cc.d: src\main.rs
+
+C:\Users\Tudor Alexie\Desktop\Rust\hello_world\target\debug\deps\libhello_world-6a1afbb608fd85cc.rmeta: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+c:\Users\Tudor\ Alexie\Desktop\Rust\hello_world\target\debug\hello_world.exe: c:\Users\Tudor\ Alexie\Desktop\Rust\hello_world\src\main.rs
