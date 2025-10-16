7ceb77037cc768d174a100a08ed472aeb98161a3
AlexieTudor <tudoralexie@gmail.com>
2025-10-16 15:02:08 +0300

    lab03

--- lab03/Probl2/src/main.rs
+++ lab03/Probl2/src/main.rs
@@ -1,49 +1,25 @@
-
-
-fn checked_add(a:u32, b:u32) ->Option<u32>{
-    let sum= (a as u64) + (b as u64);
-    if sum <= u32::MAX as u64
-    {
-        Some(sum as u32)
-    }
-    else
-    {
-        None
+fn checked_add(a: u32, b: u32) -> u32 {
+    let sum = (a as u64) + (b as u64);
+    if sum <= u32::MAX as u64 {
+        sum as u32
+    } else {
+        panic!("Overflow detectat in adunare!")
     }
 }
 
-fn checked_mul(a:u32, b:u32) ->Option<u32>{
-    let product= (a as u64) * (b as u64);
-    if product <= u32::MAX as u64
-    {
-        Some(product as u32)
-    }
-    else
-    {
-        None
+fn checked_mul(a: u32, b: u32) -> u32 {
+    let product = (a as u64) * (b as u64);
+    if product <= u32::MAX as u64 {
+        product as u32
+    } else {
+        panic!("Overflow detectat in inmultire!")
     }
 }
 
 fn main() {
-  
-   match checked_add(u32::MAX, 1) {
-        Some(result) => println!("100 + 200 = {}", result),
-        None => println!("Overflow detectat"),
-    }
+    let sum = checked_add(100, 200);
+    println!("100 + 200 = {}", sum);
 
-    match checked_add(100, 200) {
-        Some(result) => println!("100 + 200 = {}", result),
-        None => println!("Overflow detectat"),
-    }
-
-    match checked_mul(u32::MAX, 2) {
-        Some(result) => println!("100 * 200 = {}", result),
-        None => println!("Overflow detectat"),
-    }
-
-    match checked_mul(100, 200) {
-        Some(result) => println!("100 * 200 = {}", result),
-        None => println!("Overflow detectat"),
-    }
-
+    let product = checked_mul(100, 200);
+    println!("100 * 200 = {}", product);
 }
